27b355ca4bc5a61fe8cd0d7051570468c4e44c54
mhasannadeem <hasannadeem@folio3.com>
2023-02-14 02:49:24 +0500

    added key for list

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,6 +9,7 @@ function Nav() {
       <div className="flex px-4 space-x-10 sm:space-x-20 overflow-x-scroll whitespace-nowrap text-2xl scrollbar-hide">
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
+            key={key}
             className="cursor-pointer last:pr-24 transition duration-100 hover:text-white hover:scale-125 active:text-red-500"
             onClick={() => router.push(`/?genre=${key}`)}
           >
